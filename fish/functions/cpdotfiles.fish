dcfb4eba8c5c9c6e895f71c5f59ef553423a5cb8
hkgnp <ben@pngs.cc>
2022-04-06 09:37:55 +0800

    Updated dotfiles

--- fish/functions/cpdotfiles.fish
+++ fish/functions/cpdotfiles.fish
@@ -2,7 +2,7 @@ function cpdotfiles
     rsync -av --exclude ".DS_Store" ~/.config/nvim ~/Code\ Projects/dotfiles/
     rsync -av --exclude ".DS_Store" ~/.config/yabai ~/Code\ Projects/dotfiles/
     rsync -av --exclude ".DS_Store" ~/.config/skhd ~/Code\ Projects/dotfiles/
-    rsync -av ~/.config/coc/extensions/package.json ~/Code\ Projects/dotfiles/coc/extensions/package.json
+    rsync -vz ~/.config/coc/extensions/package.json ~/Code\ Projects/dotfiles/coc/extensions/
     rsync -av --exclude ".DS_Store" ~/.config/iterm2 ~/Code\ Projects/dotfiles/
     rsync -av --exclude ".DS_Store" --exclude 'z.fish' --exclude '__z*.*' ~/.config/fish ~/Code\ Projects/dotfiles
     cd ~/Code\ Projects/dotfiles
