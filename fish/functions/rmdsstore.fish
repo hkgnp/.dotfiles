3f32466fe8b8f6fe299788c1a9fe1e81b8b3d075
hkgnp <ben@pngs.cc>
2022-05-29 14:21:15 +0800

    Updated dotfiles

--- fish/functions/rmdsstore.fish
+++ fish/functions/rmdsstore.fish
@@ -1,5 +1,3 @@
 function rmdsstore
-    find / -name '.DS_Store' -type f -delete -print
-    find ~/Code_Projects/ -name '.DS_Store' -type f -delete -print
-    find ~/.config/ -name '.DS_Store' -type f -delete -print
+    find /Users/ben -name '.DS_Store' -type f -delete -print
 end
